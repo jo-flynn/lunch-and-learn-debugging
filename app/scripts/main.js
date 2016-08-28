500157166f45d5dc7f639c7ee28a9cc6320b83b7
Joe Flynn <joe@viastudio.com>
2016-08-28 18:48:04 -0400

    talk examples

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,35 @@
-console.log('\'Allo \'Allo!');
+var doSomeDOMStuff = function (foo, bar) {
+    var someContent = foo.html();
+
+    bar.html(someContent);
+
+    return bar;
+};
+
+var doSomeAjax = function () {
+    $.get('http://jsonplaceholder.typicode.com/posts/1').then(function (response) {
+        var container = $('#lorem');
+        container.find('.title').text(response.title);
+        container.find('.content').text(response.body);
+    });
+};
+
+$(document).ready(function () {
+    var foo = $('#foo');
+    var bar = $('#bar');
+
+    foo.text('testing');
+
+    // Debug Statements
+    console.log(foo);
+
+    // Inline breakpoints
+    // debugger;
+
+    // Step through debugging
+    bar = doSomeDOMStuff(foo, bar);
+
+    // Network debugging
+    doSomeAjax();
+});
+
